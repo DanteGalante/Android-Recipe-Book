f287a695d6ebafccf5619ee93e1237314ba37393
Dan <olveradan@hotmail.com>
2021-11-05 09:57:22 -0600

    Recipe editing menu

---
+++ RecipeBook/app/src/main/java/uv/recipebook/controllers/EditRecipeMenu.kt
@@ -0,0 +1,11 @@
+package uv.recipebook.controllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditRecipeMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_recipe_menu)
+    }
+}
--- RecipeBook/app/src/main/java/uv/recipebook/utils/RecyclerAdapter_Ingredient.kt
+++ RecipeBook/app/src/main/java/uv/recipebook/utils/RecyclerAdapter_Ingredient.kt
@@ -1,10 +1,13 @@
 package uv.recipebook.utils
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import uv.recipebook.data.Ingredient
 import uv.recipebook.R
@@ -13,14 +16,15 @@ class RecyclerAdapter_Ingredient : RecyclerView.Adapter<RecyclerAdapter_Ingredie
     var ingredientList: MutableList<Ingredient> = ArrayList()
     lateinit var context: Context
 
-    fun addIngrediente(ingredient: Ingredient) {
-        ingredientList.add(ingredient)
-        notifyItemInserted(itemCount)
-    }
-
     fun RecyclerAdapter_Ingredient(ingredients: MutableList<Ingredient>, context: Context){
         this.ingredientList = ingredients
         this.context = context
+        notifyDataSetChanged()
+    }
+
+    fun addIngrediente(ingredient: Ingredient) {
+        ingredientList.add(ingredient)
+        notifyItemInserted(itemCount)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder_Ingredient {
@@ -31,15 +35,8 @@ class RecyclerAdapter_Ingredient : RecyclerView.Adapter<RecyclerAdapter_Ingredie
     override fun onBindViewHolder(holder: ViewHolder_Ingredient, position: Int) {
         val currentItem = ingredientList[position]
         holder.bind(currentItem, context)
-
-        println("\n\n${ingredientList}\n\n")
-
-        holder.itemView.findViewById<ImageButton>(R.id.ib_deleteIngredient).setOnClickListener {
-            Toast.makeText(context, "Ingrediente eliminado",Toast.LENGTH_SHORT).show()
-            ingredientList.remove(currentItem)
-            notifyItemRemoved(position)
-            notifyDataSetChanged()
-        }
+        holder.delete(currentItem, position, context, this)
+        holder.listenEdits(this)
     }
 
     override fun getItemCount(): Int {
@@ -49,10 +46,62 @@ class RecyclerAdapter_Ingredient : RecyclerView.Adapter<RecyclerAdapter_Ingredie
     class ViewHolder_Ingredient(view: View) : RecyclerView.ViewHolder(view) {
         val ingredientName = view.findViewById<EditText>(R.id.et_ingredientName)
         val amountGr = view.findViewById<EditText>(R.id.et_amountGr)
+        val delete = view.findViewById<ImageButton>(R.id.ib_deleteIngredient)
 
         fun bind(ingredient: Ingredient, context: Context) {
             ingredientName.setText(ingredient.ingredient)
             amountGr.setText(ingredient.amountInGr.toString())
+        }
+
+        fun listenEdits(adapter: RecyclerAdapter_Ingredient) {
+            ingredientName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    adapter.ingredientList[adapterPosition].ingredient = s.toString()
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+
+            })
+            amountGr.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if(s!!.isBlank()) {
+                        adapter.ingredientList[adapterPosition].amountInGr = 0
+                    } else {
+                        adapter.ingredientList[adapterPosition].amountInGr = s.toString().toInt()
+                    }
+
+                    println("id = $adapterPosition")
+                    println("amount in gr = ${adapter.ingredientList[adapterPosition].amountInGr}")
+                    println("ingredient list:")
+                    println(adapter.ingredientList)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    println("AFTER TEXT CHANGED")
+                    println("id = $adapterPosition")
+                    //adapter.notifyItemInserted(adapterPosition)
+                }
+
+            })
+        }
+
+        fun delete(ingredient: Ingredient, position: Int, context: Context, adapter: RecyclerAdapter_Ingredient) {
+            delete.setOnClickListener {
+                Toast.makeText(context, "Ingrediente eliminado",Toast.LENGTH_SHORT).show()
+                adapter.ingredientList.remove(ingredient)
+                adapter.notifyItemRemoved(position)
+                adapter.notifyDataSetChanged()
+            }
         }
     }
 }
