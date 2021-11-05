15cf1b50ecd8cb18730f9c0129d25ea7d4204aad
Dan <olveradan@hotmail.com>
2021-11-05 10:15:33 -0600

    Added recipe displaying menu

---
+++ RecipeBook/app/src/main/java/uv/recipebook/controllers/RecipeDisplayMenu.kt
@@ -0,0 +1,11 @@
+package uv.recipebook.controllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeDisplayMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_display_menu)
+    }
+}
